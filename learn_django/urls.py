f0e3d116e48e122631f14d1af12f121a81d77590
M1dor1ya <30382550+M1dor1ya@users.noreply.github.com>
2019-03-15 10:56:27 +0800

    Update urls.py

--- learn_django/urls.py
+++ learn_django/urls.py
@@ -3,12 +3,15 @@
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import verify_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app1/', include('app1.urls')),
     path('rest/', include('restful_app.urls')),
     path('auth/', include('user_auth.urls')),
+    path(r'api-auth/', include('rest_framework.urls')),  # 对所有rest_framework开启认证
+    path(r'api-token-verify/', verify_jwt_token),
 ]
 
 """
