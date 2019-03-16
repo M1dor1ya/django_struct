dca5d40c56ce83744a489426cee4a575c210772f
M1dor1ya <30382550+M1dor1ya@users.noreply.github.com>
2019-03-16 16:59:49 +0800

    Update urls.py

--- restful_app/urls.py
+++ restful_app/urls.py
@@ -2,13 +2,17 @@
 # @Time    : 2018/9/14 10:31
 # @Author  : Zcs
 # @File    : urls.py.py
-from django.conf.urls import url
+"""
+url方法为Django 1.X推荐使用的，Django2.0请使用path和re_path方法
+"""
+#  from django.conf.urls import url
+from django.conf.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    url(r'^network/dev/$', views.DevInfo.as_view()),
-    # url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    path(r'network/dev/', views.DevInfo.as_view()),
+    # path(r'snippets/(?P<pk>[0-9]+)/', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
